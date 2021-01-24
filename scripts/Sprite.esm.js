31566977da970576c14589d8ac3bc4530bcf6af5
Mathias007 <mathias.najlepszy@gmail.com>
2021-01-24 18:39:39 +0100

    Add JSDoc comments in Common & Sprite classes.

--- scripts/Sprite.esm.js
+++ scripts/Sprite.esm.js
@@ -1,6 +1,26 @@
 import { canvas } from "./Canvas.esm.js";
 
+/**
+ * @typedef {{ x: number, y: number }} Offset
+ */
+
+/**
+ * Class for creating Sprite object
+ */
+
 export class Sprite {
+    /**
+     * Create a sprite object
+     * @param {!number} spriteX The x-axis coordinate of the top left corner of the sub-rectangle of the source image to draw into the destination context.
+     * @param {!number} spriteY The y-axis coordinate of the top left corner of the sub-rectangle of the source image to draw into the destination context.
+     * @param {!number} width The width of the sub-rectangle of the source image to draw into the destination context
+     * @param {!number} height The height of the sub-rectangle of the source image to draw into the destination context.
+     * @param {!CanvasImageSource} spritesImage An element to draw into the context.
+     * @param {!number} x X position of sprite in canvas
+     * @param {!number} y Y position of sprite in canvas
+     * @param {number} [numberOfSprites = 1] Default = 1, Number of sprite images in the main image
+     * @param {Offset} [offset = {x: 0, y: 0}] Default = {x: 0, y: 0}, Offset to draw
+     */
     constructor(
         spriteX,
         spriteY,
@@ -12,6 +32,9 @@ export class Sprite {
         numberOfSprites = 1,
         offset = { x: 0, y: 0 }
     ) {
+        /**
+         * @type {!number}
+         */
         this.alpha = 255;
         this.height = height;
         this.numberOfSprites = numberOfSprites;
@@ -23,6 +46,12 @@ export class Sprite {
         this.x = x;
         this.y = y;
     }
+
+    /**
+     * Method draw sprite into the canvas context
+     * @param {number} [numberOfSprites = 0] Number of index sprite to draw
+     * @param {number} [ratio = 1] Scale if it is different than 1
+     */
 
     draw(numberOfSprites = 0, ratio = 1) {
         if (numberOfSprites > this.numberOfSprites) {
